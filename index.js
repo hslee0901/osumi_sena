4742cf7567c8c612dff4b5b540bc9ffe140d130a
hslee0901 <133766427+hslee0901@users.noreply.github.com>
2024-12-30 22:26:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,13 @@ document.getElementById('pause-button').addEventListener('click', function() {
     const audio = document.getElementById('audio');
     audio.pause();
 });
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleAsideButton = document.getElementById('toggle-aside');
-    const closeAsideButton = document.getElementById('close-aside');
-    const sidebar = document.getElementById('sidebar');
 
-    toggleAsideButton.addEventListener('click', function() {
-        sidebar.style.display = 'block'; // 사이드바 표시
-        toggleAsideButton.style.display = 'none'; // 열기 버튼 숨김
-    });
-
-    closeAsideButton.addEventListener('click', function() {
-        sidebar.style.display = 'none'; // 사이드바 숨김
-        toggleAsideButton.style.display = 'block'; // 열기 버튼 표시
-    });
-});
+        $(document).ready(function() {
+            var currentUrl = window.location.pathname;
+            $('.lang-link').each(function() {
+                var linkUrl = $(this).attr('href');
+                if (linkUrl === currentUrl) {
+                    $(this).addClass('active');
+                }
+            });
+        });
